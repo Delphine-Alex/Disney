88cfae9610acb0d000a1fe376a6e80cd4feaae47
Delphine <delphine.pva@gmail.com>
2022-10-27 13:14:20 +0200

    Cleaning code on Routes component"

--- src/configuration/routes.js
+++ src/configuration/routes.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-// import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+// Create a modal
+// import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from '../screens/Home';
-// import HomeStack from '../configuration/homeStack';
-
 import Search from '../screens/Search';
-import Downloads from '../screens/Downloads';
+// import Downloads from '../screens/Downloads';
 import Profiles from '../screens/Profiles';
 
 const Routes = () => {
   const BottomStack = createMaterialBottomTabNavigator();
+  // const RootStack = createStackNavigator();
 
   return (
     <NavigationContainer>
@@ -21,27 +22,10 @@ const Routes = () => {
           headerShown: false,
         }}
       >
-        <BottomStack.Screen
-          options={{
-            tabBarLabel: 'Test',
-          }}
-          name="Home"
-          component={Home} />
-        {/* <BottomStack.Screen name="HomeStack" component={HomeStack} /> */}
-        <BottomStack.Screen
-          options={{
-            tabBarLabel: 'Test1',
-          }}
-          name="Search" component={Search} />
-        <BottomStack.Screen
-          options={{
-            tabBarLabel: 'Test2',
-          }}
-          name="Downloads" component={Downloads} />
-        <BottomStack.Screen
-          options={{
-            tabBarLabel: 'Test3',
-          }} name="Profiles" component={Profiles} />
+        <BottomStack.Screen name="Home" component={Home} />
+        <BottomStack.Screen name="Search" component={Search} />
+        {/* <BottomStack.Screen name="Downloads" component={Downloads} /> */}
+        <BottomStack.Screen name="Profiles" component={Profiles} />
       </BottomStack.Navigator>
     </NavigationContainer>
   );
