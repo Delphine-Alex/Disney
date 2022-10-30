1b3f826a11cf5a04ff7ae890fa010d2baa734fbc
Delphine <delphine.pva@gmail.com>
2022-10-30 15:31:28 +0100

    Project done

--- src/configuration/bottomNavigator.js
+++ src/configuration/bottomNavigator.js
@@ -6,15 +6,31 @@ import Home from '../screens/Home';
 import Search from '../screens/Search';
 import Profile from '../screens/Profile';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 const BottomNavigator = () => {
   const BottomStack = createMaterialBottomTabNavigator();
 
   return (
-    <BottomStack.Navigator>
+    <BottomStack.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home'
+          } else if (route.name === 'Search') {
+            iconName = 'search-outline'
+          } else {
+            iconName = 'person-circle-outline'
+          }
+          return <Icon name={iconName} size={20} color={color} />;
+        },
+      })}
+    >
       <BottomStack.Screen name="Home" component={Home} />
       <BottomStack.Screen name="Search" component={Search} />
       <BottomStack.Screen name="Profile" component={Profile} />
-    </BottomStack.Navigator>
+    </BottomStack.Navigator >
   )
 }
 
--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 
+import { Avatar } from 'react-native-elements';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button, ScrollView, Text, View } from 'react-native';
+import { Button, ScrollView, Text, Touchable, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import verifyIfUserIsConnected from '../../utils/verifyIfUserIsConnected';
 
+import Raiponce from '../../assets/Raiponce.jpeg';
+
 import styled from 'styled-components';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 const Profile = () => {
@@ -23,14 +28,30 @@ const Profile = () => {
 
   return (
     <Container>
-      <Button title='Ma Liste' onPress={() => navigation.navigate('Favorite')} />
-      <View
-        style={{
-          borderBottomColor: 'black',
-          // borderBottomWidth: StyleSheet.hairlineWidth,
-        }}
-      />
-      <Button title='Se déconnecter' onPress={disconnect} />
+
+      <Picture>
+        <Avatar
+          rounded
+          source={Raiponce}
+          size={125}
+        />
+      </Picture>
+
+      <TouchableOpacity onPress={() => navigation.navigate('Favorite')}>
+        <Content>
+          <Title>Ma liste</Title>
+          <Icon name='ios-arrow-forward-circle' size={25} />
+        </Content>
+      </TouchableOpacity>
+
+      <Diviser />
+
+      <TouchableOpacity onPress={disconnect}>
+        <Content>
+          <Title>Se déconnecter</Title>
+          <Icon name='ios-arrow-forward-circle' size={25} />
+        </Content>
+      </TouchableOpacity>
     </Container >
   );
 }
@@ -40,5 +61,31 @@ const Container = styled.View`
   padding: 6%;
   height: 100%;
 `
+
+const Picture = styled.View`
+  padding: 25%;
+  align-items: center;
+`
+
+const Title = styled.Text`
+  color: ${props => props.theme.primaryColor};
+  padding: 2% 0 2% 0;
+  font-weight: 600;
+  font-size: 16px;
+`
+
+const Content = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Diviser = styled.View`
+  background-color: ${props => props.theme.primaryColor};
+  border-bottom-width: 2px;
+  margin: 2% 0 2% 0;
+`
+
 
 export default Profile;
